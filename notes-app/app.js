921af17e35bac0614fb1b34667e799afbd134111
AltaraceO <orialtarace@gmail.com>
2022-01-09 14:17:10 +0200

    file system

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,3 +2,8 @@ const fs = require("fs");
 
 // fs.writeFileSync("notes.txt", "my name is Ori");
 fs.appendFileSync("notes.txt", "-- and I am from here");
+fs.writeFileSync("more_notes.txt", "more text");
+fs.copyFileSync("more_notes.txt", "extra.txt");
+fs.renameSync("extra.txt", "new_extra.txt");
+
+fs.readdirSync("../notes-app").forEach((file) => console.log(file));
