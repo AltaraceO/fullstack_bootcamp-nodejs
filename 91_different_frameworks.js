1b984d26e3a07d9813ccac6e333ce2e1785b7bc2
AltaraceO <orialtarace@gmail.com>
2022-01-14 01:24:20 +0200

    end of day

---
+++ 91_different_frameworks.js
@@ -0,0 +1,16 @@
+//!koa
+// Koa is a new web framework designed by the team behind Express, which aims to be a smaller, more expressive, and more robust foundation for web applications and APIs. By leveraging async functions, Koa allows you to ditch callbacks and greatly increase error-handling.
+
+// Koa does not bundle any middleware within its core, and it provides an elegant suite of methods that make writing servers fast and enjoyable. It scores quite impressive stars on GitHub: +25000, earning approximately 300000 npm weekly downloads as of today despite its being one of the newborns in the Node.js game.
+
+//!Hapi
+
+// Hapi is a feature-rich framework that favours configuration over code and attempts to cover a wider range of use cases out of the box. It was originally created by a member of WalmartLabs, and it is intended for large teams and large projects. Because of this, it can be a bit boilerplate-heavy for small projects.
+
+// While Hapi is less widely used and slightly newer than Express (Hapi started development in 2011), Hapi is used by a variety of large companies such as Walmart, Disney, and Macy’s. It has more than 11000 stars on GitHub and over 250000 npm downloads each week.
+
+//!Express
+
+// Express is a code-centric web application framework that aims to provide developers with a simple, performant, and unopinionated toolset for creating web application servers. The API is kept lightweight and maintains a high degree of consistency with the NodeJS core API. Because of its minimalist nature, many common tasks require outside modules.
+
+// Having been in development since 2009, Express is a mature project with a strong community backing. With +43000 stars on GitHub and about 8 millions npm weekly download, Express has proved its reign as the most loved and widely used Node.js web application framework. The latest version of Express is 4.17.1 published in May 2019. The publisher also announced on its 5.0 version, which is currently in the alpha stage, promising the upcoming dawn of Express 5 universe.
